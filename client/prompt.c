e044534221b23950e0f5c16103513896ae3266cb
Denis MOUREU-LARRANG <dmoureu-@e1r9p5.42.fr>
2017-10-06 16:04:18 +0200

    OK

--- client/prompt.c
+++ client/prompt.c
@@ -6,16 +6,17 @@
 /*   By: dmoureu- <dmoureu-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/06 19:08:28 by dmoureu-          #+#    #+#             */
-/*   Updated: 2017/10/04 12:57:20 by dmoureu-         ###   ########.fr       */
+/*   Updated: 2017/10/06 16:04:07 by dmoureu-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "client.h"
 
 void make_buffer(struct input_line *buf) {
-    buf->ln = NULL;
+    buf->ln = malloc(PROMPT_SIZE_MAX+1);
+	bzero(buf->ln, PROMPT_SIZE_MAX+1);
     buf->length = 0;
-    buf->capacity = 0;
+    buf->capacity = PROMPT_SIZE_MAX;
     buf->cursor = 0;
     buf->last_rendered = 0;
 }
@@ -25,30 +26,43 @@ void destroy_buffer(struct input_line *buf) {
     make_buffer(buf);
 }
 
-void render_line(struct input_line *buf, WINDOW *win) {
+void render_line(struct input_line *buf, WINDOW *win)
+{
     int i = 0;
-    for(; i < buf->length; i ++) {
+
+    while(i < buf->length)
+    {
         chtype c = buf->ln[i];
-        if(i == buf->cursor) {
+        if(i == buf->cursor)
+        {
             c |= A_REVERSE;
         }
         waddch(win,c);
+        i++;
     }
-    if(buf->cursor == buf->length) {
+    if(buf->cursor == buf->length)
+    {
         waddch(win,' ' | A_REVERSE);
-        i ++;
+        i++;
     }
     int rendered = i;
-    // Erase previously rendered characters
-    for(; i < buf->last_rendered; i ++) {
+    while(i < buf->last_rendered)
+    {
         waddch(win,' ');
+        i++;
     }
     buf->last_rendered = rendered;
 }
 
-int retrieve_content(struct input_line *buf, char *target, int max_len) {
-    int len = buf->length < (max_len - 1) ? buf->length : (max_len - 1);
-    memcpy(target, buf->ln, len);
+int retrieve_content(struct input_line *buf, char *target, int max_len)
+{
+    int len;
+
+    if (buf->length < (max_len - 1))
+        len = buf->length;
+    else
+        len = (max_len - 1);
+    ft_memcpy(target, buf->ln, len);
     target[len] = '\0';
     buf->cursor = 0;
     buf->length = 0;
@@ -56,27 +70,52 @@ int retrieve_content(struct input_line *buf, char *target, int max_len) {
 }
 
 void add_char(struct input_line *buf, char ch) {
-    // Ensure enough space for new character
-    if(buf->length == buf->capacity) {
-        int ncap = buf->capacity + 1024;
-        char *nln = (char*) realloc(buf->ln, ncap);
-        if(!nln) {
-            // Out of memory!
-            return;
-        }
-        buf->ln = nln;
-        buf->capacity = ncap;
-    }
+    int ncap;
+    char *nln;
 
-    // Add new character
-    memmove(
+    if(buf->length == buf->capacity)
+        return ;
+    ft_memmove(
         &buf->ln[buf->cursor+1],
         &buf->ln[buf->cursor],
         buf->length - buf->cursor
     );
     buf->ln[buf->cursor] = ch;
-    ++ buf->cursor;
-    ++ buf->length;
+    ++buf->cursor;
+    ++buf->length;
+}
+
+
+int handle_input_key(struct input_line *buf, int key)
+{
+    if (key == KEY_HOME)
+        buf->cursor = 0;
+    if (key == KEY_END)
+        buf->cursor = buf->length;
+    if (key == '\t')
+        add_char(buf, '\t');
+    if (key == KEY_BACKSPACE || key == 127 || key == 8)
+	{
+        if (buf->cursor <= 0)
+		{
+            return (0);
+		}
+        else
+        {
+            buf->cursor--;
+            key = KEY_DC;
+        }
+	}
+    if (key == KEY_DC)
+        if(buf->cursor < buf->length) {
+            ft_memmove(
+                &buf->ln[buf->cursor],
+                &buf->ln[buf->cursor+1],
+                buf->length - buf->cursor - 1
+            );
+            buf->length --;
+        }
+    return (0);
 }
 
 int handle_input(struct input_line *buf, char *target, int max_len, int key) {
@@ -86,47 +125,24 @@ int handle_input(struct input_line *buf, char *target, int max_len, int key) {
         add_char(buf, key);
         return 0;
     }
-
-    switch(key) {
-    case ERR: /* no key pressed */ break;
-    case KEY_LEFT:  if(buf->cursor > 0)           { buf->cursor --; } break;
-    case KEY_RIGHT: if(buf->cursor < buf->length) { buf->cursor ++; } break;
-    case KEY_HOME:  buf->cursor = 0;           break;
-    case KEY_END:   buf->cursor = buf->length; break;
-    case '\t':
-        add_char(buf, '\t');
-        break;
-    case KEY_BACKSPACE:
-    case 127:
-    case 8:
-        if(buf->cursor <= 0) {
-            break;
-        }
-        buf->cursor --;
-        // Fall-through
-    case KEY_DC:
-        if(buf->cursor < buf->length) {
-            memmove(
-                &buf->ln[buf->cursor],
-                &buf->ln[buf->cursor+1],
-                buf->length - buf->cursor - 1
-            );
-            buf->length --;
-        }
-        break;
-    case KEY_ENTER:
-    case '\r':
-    case '\n':
+    if (key == ERR)
+        return (0);
+    if (key == KEY_LEFT)
+        if(buf->cursor > 0)
+            buf->cursor --;
+    if (key == KEY_RIGHT)
+        if (buf->cursor < buf->length)
+            buf->cursor ++;
+    handle_input_key(buf, key);
+    if (key == KEY_ENTER || key == '\r' || key == '\n')
         return retrieve_content(buf, target, max_len);
-    }
-    return 0;
+    return (0);
 }
 
 int get_line_non_blocking(struct input_line *buf, char *target, int max_len) {
     while(1) {
         int key = getch();
         if(key == ERR) {
-            // No more input
             return 0;
         }
         int n = handle_input(buf, target, max_len, key);
